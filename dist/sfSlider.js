684089b1bf1595d017fbd80d17c97deebe75746a
faraz <frzk410@gmail.com>
2016-08-12 13:55:56 +0500

    simplified usage and added usefull classes

--- dist/sfSlider.js
+++ dist/sfSlider.js
@@ -1,15 +1,27 @@
-angular.module('templates', []).run(['$templateCache', function($templateCache) {$templateCache.put('src/templates/sfSlider.html','<div>\n<!-- Surrounding DIV for sfField builder to add a sfField directive to. -->\n\n  <h2>{{form.myOwnFormOption}}</h2>\n  <!-- Create your own form options -->\n\n  <label>{{form.title}}</label>\n  <slider sf-field-model ticks="form.keys" min="form.min" max="form.max" ticks-labels="form.values" tooltip="hide"></slider>\n  <!-- sf-field-model let\'s the ngModel builder know that you want a ng-model that matches against the form key here -->\n  <!-- schema-validate="form" validates the form against the schema -->\n\n  <span sf-message="form.description"></span>\n  <!-- Description & Validation messages -->\n\n</div>\n');}]);
+angular.module('templates', []).run(['$templateCache', function($templateCache) {$templateCache.put('src/templates/sfSlider.html','<div class="form-group {{form.htmlClass}}">\n  <label class="control-label {{form.labelHtmlClass}}" ng-show="showTitle()">{{form.title}}</label>\n  <slider sf-field-model ticks="form.keys"\n  min="form.min" max="form.max"\n  ticks-labels="form.values" tooltip="hide"></slider>\n  <span class="help-block"  sf-message="form.description"></span>\n\n</div>\n');}]);
 angular.module('sfSlider', [
   'schemaForm',
   'templates',
   'ui.bootstrap-slider'
-]).config(function(schemaFormDecoratorsProvider, sfBuilderProvider) {
-
+]).config(function(schemaFormDecoratorsProvider, sfBuilderProvider, schemaFormProvider, sfPathProvider) {
+  var slider = function(name, schema, options){
+    var f = schemaFormProvider.stdFormObj(name, schema, options);
+    if(schema.enum){
+      f.keys = schema.enum.map(function (value, key) {
+        return key;
+      })
+      f.values = schema.enum.map(function (value, key) {
+        return value;
+      })
+    }
+    options.lookup[sfPathProvider.stringify(options.path)] = f;
+    return f;
+  };
+  schemaFormProvider.defaults.string.unshift(slider);
   schemaFormDecoratorsProvider.defineAddOn(
     'bootstrapDecorator',           // Name of the decorator you want to add to.
     'sfSlider',                      // Form type that should render this add-on
     'src/templates/sfSlider.html',  // Template name in $templateCache
     sfBuilderProvider.stdBuilders   // List of builder functions to apply.
   );
-
 });
--- src/module.js
+++ src/module.js
@@ -2,13 +2,25 @@ angular.module('sfSlider', [
   'schemaForm',
   'templates',
   'ui.bootstrap-slider'
-]).config(function(schemaFormDecoratorsProvider, sfBuilderProvider) {
-
+]).config(function(schemaFormDecoratorsProvider, sfBuilderProvider, schemaFormProvider, sfPathProvider) {
+  var slider = function(name, schema, options){
+    var f = schemaFormProvider.stdFormObj(name, schema, options);
+    if(schema.enum){
+      f.keys = schema.enum.map(function (value, key) {
+        return key;
+      })
+      f.values = schema.enum.map(function (value, key) {
+        return value;
+      })
+    }
+    options.lookup[sfPathProvider.stringify(options.path)] = f;
+    return f;
+  };
+  schemaFormProvider.defaults.string.unshift(slider);
   schemaFormDecoratorsProvider.defineAddOn(
     'bootstrapDecorator',           // Name of the decorator you want to add to.
     'sfSlider',                      // Form type that should render this add-on
     'src/templates/sfSlider.html',  // Template name in $templateCache
     sfBuilderProvider.stdBuilders   // List of builder functions to apply.
   );
-
 });
