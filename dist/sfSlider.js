6732a4f43503f67de57b93abbd30e1481c2b60a6
Faraz <frzk410@gmail.com>
2016-11-11 17:22:31 +0500

    added change event

--- dist/sfSlider.js
+++ dist/sfSlider.js
@@ -1,4 +1,4 @@
-angular.module('templates', []).run(['$templateCache', function($templateCache) {$templateCache.put('src/templates/sfSlider.html','<div class="form-group {{form.htmlClass}}">\n  <label class="control-label {{form.labelHtmlClass}}" ng-show="showTitle()">{{form.title}}</label>\n  <slider ng-model="$$value$$" ticks="form.keys"\n  ng-model-options="form.ngModelOptions"\n  min="form.min" max="form.max"\n  ticks-labels="form.values" tooltip="hide"></slider>\n  <span class="help-block"  sf-message="form.description"></span>\n\n</div>\n');}]);
+angular.module('templates', []).run(['$templateCache', function($templateCache) {$templateCache.put('src/templates/sfSlider.html','<div class="form-group {{form.htmlClass}}">\n  <label class="control-label {{form.labelHtmlClass}}" ng-show="showTitle()">{{form.title}}</label>\n  <slider ng-model="$$value$$" ticks="form.keys"\n  ng-model-options="form.ngModelOptions"\n  sf-changed="form"\n  min="form.min" max="form.max"\n  ticks-labels="form.values" tooltip="hide"></slider>\n  <span class="help-block"  sf-message="form.description"></span>\n\n</div>\n');}]);
 angular.module('sfSlider', [
   'schemaForm',
   'templates',
